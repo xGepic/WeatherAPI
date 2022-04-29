d2b4d68f1a6ee95674ef89e62ca6182ff20fb6a6
Steve <simanek.s@gmx.at>
2022-04-29 15:28:44 +0200

    Revert "Added Test Proj"
    This reverts commit 5b66fa2ae7ae5f700137c5d3b5b7101067f0c6f8.

--- WeatherAPITests/Global Usings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.Extensions.Logging;
-global using Moq;
-global using NUnit.Framework;
-global using System.Collections.Generic;
-global using WeatherAPI;
-global using WeatherAPI.Controllers;
