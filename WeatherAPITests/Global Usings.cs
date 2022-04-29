5b66fa2ae7ae5f700137c5d3b5b7101067f0c6f8
Steve <simanek.s@gmx.at>
2022-04-29 15:22:38 +0200

    Added Test Proj

---
+++ WeatherAPITests/Global Usings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.Extensions.Logging;
+global using Moq;
+global using NUnit.Framework;
+global using System.Collections.Generic;
+global using WeatherAPI;
+global using WeatherAPI.Controllers;
